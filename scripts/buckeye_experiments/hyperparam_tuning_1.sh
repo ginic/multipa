21b591d2e007db874c07271f945d6949ba4a11e4
Virginia Partridge <vcpartridge@umass.edu>
2024-05-07 22:06:31 +0000

    correct epochs arg

--- scripts/buckeye_experiments/hyperparam_tuning_1.sh
+++ scripts/buckeye_experiments/hyperparam_tuning_1.sh
@@ -22,6 +22,6 @@ module load cuda/11.3.1
 
 conda activate multipa
 
-multipa-train --output_dir "$model_dir" --data_dir "$data_dir" --no_space --cache_dir "$dataset_cache" --use_gpu --num_training_epochs 2 --num_proc 12 \
+multipa-train --output_dir "$model_dir" --data_dir "$data_dir" --no_space --cache_dir "$dataset_cache" --use_gpu --num_train_epochs 2 --num_proc 12 \
     --learning_rate $learning_rate --per_device_train_batch_size $batch_size --gradient_accumulation_steps $grad_acc \
     buckeye --train_samples 2000 --val_samples 5605
