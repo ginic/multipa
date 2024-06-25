3521ddc308ff054c92b05efab21bdae34ad8a460
Virginia Partridge <vcpartridge@umass.edu>
2024-06-25 16:06:48 +0000

    Increase cpus and timeouts for varying individuals experiments

--- scripts/buckeye_experiments/vary_individuals_young_only_3.sh
+++ scripts/buckeye_experiments/vary_individuals_young_only_3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH -c 8
+#SBATCH -c 12
 #SBATCH --mem=16GB
 #SBATCH -p gpu-preempt
 #SBATCH -G 4
 #SBATCH --constraint=[a100|m40|rtx8000]
-#SBATCH --time 16:00:00
+#SBATCH --time 24:00:00
 #SBATCH -o train_vary_individuals_young_only_3.out
 #SBATCH --mail-type END
 
@@ -25,7 +25,7 @@ conda activate multipa
 
 python --version
 
-multipa-train --output_dir "$model_dir" --data_dir "$data_dir" --no_space --cache_dir "$dataset_cache" --use_gpu --num_train_epochs 10 --num_proc 8 \
+multipa-train --output_dir "$model_dir" --data_dir "$data_dir" --no_space --cache_dir "$dataset_cache" --use_gpu --num_train_epochs 10 --num_proc 12 \
     --learning_rate $learning_rate --per_device_train_batch_size $batch_size --gradient_accumulation_steps $grad_acc --mask_time_length 4 \
     --train_seed 103 \
     buckeye --train_samples 4000 --val_samples 5605 \
