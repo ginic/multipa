2a1046843e3f7483b2d8b4fcfceb2aa15c795aa2
Virginia Partridge <vcpartridge@umass.edu>
2024-08-09 15:58:29 +0000

    Attempts to recognize GPUs in slurm scripts

--- scripts/buckeye_experiments/vary_individuals_young_only_1.sh
+++ scripts/buckeye_experiments/vary_individuals_young_only_1.sh
@@ -3,7 +3,8 @@
 #SBATCH -c 12
 #SBATCH --mem=24GB
 #SBATCH -p gpu-preempt
-#SBATCH -G 4
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-task=4
 #SBATCH --constraint=[a100|m40|rtx8000]
 #SBATCH --time 24:00:00
 #SBATCH -o train_vary_individuals_young_only_1.out
@@ -24,6 +25,7 @@ module load cuda/11.8.0
 conda activate multipa
 
 python --version
+
 
 multipa-train --output_dir "$model_dir" --data_dir "$data_dir" --no_space --cache_dir "$dataset_cache" --use_gpu --num_train_epochs 10 --num_proc 12 \
     --learning_rate $learning_rate --per_device_train_batch_size $batch_size --gradient_accumulation_steps $grad_acc --mask_time_length 4 \
--- scripts/buckeye_experiments/vary_individuals_young_only_3.sh
+++ scripts/buckeye_experiments/vary_individuals_young_only_3.sh
@@ -24,6 +24,8 @@ module load cuda/11.8.0
 conda activate multipa
 
 python --version
+echo "How many GPUS found by pytorch?"
+python -c "import torch; print(torch.cuda.device_count())"
 
 multipa-train --output_dir "$model_dir" --data_dir "$data_dir" --no_space --cache_dir "$dataset_cache" --use_gpu --num_train_epochs 10 --num_proc 12 \
     --learning_rate $learning_rate --per_device_train_batch_size $batch_size --gradient_accumulation_steps $grad_acc --mask_time_length 4 \
