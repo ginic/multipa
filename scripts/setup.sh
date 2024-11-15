b00ac35e218da94cb20fec87bc6ce6be17f39869
Virginia Partridge <vcpartridge@gmail.com>
2024-11-15 18:12:02 -0500

    Attempting to update pytorch version, not clear if this will work on Unity yet

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,6 @@
 # Installation and download IPA dictionaries
 
 pip install --upgrade pip
-pip install torch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0
+pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0
 pip install .[dev,test]
 python -m unidic download
