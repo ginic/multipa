e77c2be320ecb0e040e9ed3db04749b9de65aef4
Virginia Partridge <vcpartridge@gmail.com>
2024-11-18 15:39:54 -0500

    Update versions in requirements.txt to match pyproject.toml

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Installation and download IPA dictionaries
+# Installation for non-GPU and download IPA dictionaries
 
 pip install --upgrade pip
 pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0
