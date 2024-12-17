faee0108a3896e35bbc661db435491db32c1ebbf
Virginia Partridge <vcpartridge@umass.edu>
2024-12-17 17:31:13 +0000

    Updating evaluation results on test data post glottal stop fix

--- scripts/buckeye_experiments/data_seed_bs64_eval.sh
+++ scripts/buckeye_experiments/data_seed_bs64_eval.sh
@@ -19,10 +19,10 @@ module load conda/latest
 conda activate ./env_cuda124
 
 multipa-evaluate --local_models \
- data/models/data_seed_bs64_1/wav2vec2-large-xlsr-buckeye-ipa \
- data/models/data_seed_bs64_2/wav2vec2-large-xlsr-buckeye-ipa \
- data/models/data_seed_bs64_3/wav2vec2-large-xlsr-buckeye-ipa \
- data/models/data_seed_bs64_4/wav2vec2-large-xlsr-buckeye-ipa \
+ data/models/data_seed_bs64_1/wav2vec2-large-xlsr-53-buckeye-ipa \
+ data/models/data_seed_bs64_2/wav2vec2-large-xlsr-53-buckeye-ipa \
+ data/models/data_seed_bs64_3/wav2vec2-large-xlsr-53-buckeye-ipa \
+ data/models/data_seed_bs64_4/wav2vec2-large-xlsr-53-buckeye-ipa \
  data/models/hyperparam_tuning_1/wav2vec2-large-xlsr-53-buckeye-ipa \
  --eval_out $EVAL_RESULTS_CSV \
  --verbose_results_dir $DETAILED_RESULTS_DIR \
