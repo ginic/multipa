457d43c9907d8d53732ecd55f59b5263c126590f
Virginia Partridge <vcpartridge@gmail.com>
2025-07-22 17:47:46 -0400

    Removed --no_space flag from Buckeye experiment scripts

--- scripts/buckeye_experiments/hyperparam_tuning/hyperparam_tuning_3.sh
+++ scripts/buckeye_experiments/hyperparam_tuning/hyperparam_tuning_3.sh
@@ -24,7 +24,7 @@ conda activate ./env_cuda124
 
 python --version
 
-multipa-train --output_dir "$model_dir" --data_dir "$data_dir" --no_space --cache_dir "$dataset_cache" --use_gpu --num_train_epochs 10 --num_proc 8 \
+multipa-train --output_dir "$model_dir" --data_dir "$data_dir" --cache_dir "$dataset_cache" --use_gpu --num_train_epochs 10 --num_proc 8 \
     --learning_rate $learning_rate --per_device_train_batch_size $batch_size --gradient_accumulation_steps $grad_acc --mask_time_length 4 \
     --train_seed 42 \
     buckeye --train_samples 4000 --val_samples 5605
