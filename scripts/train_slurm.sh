152295f7423b3e1a081ec3a9b3452f5bd2a7b7c4
Virginia Partridge <vcpartridge@umass.edu>
2025-07-22 22:50:50 +0000

    Updated training test scripts to match changes to main.py

--- scripts/train_slurm.sh
+++ scripts/train_slurm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Train an audio to IPA transcription speech recognizer
 
-#SBATCH -c 12
-#SBATCH --mem=12GB
+#SBATCH -c 8
+#SBATCH --mem=32GB
 #SBATCH -p gpu-preempt
 #SBATCH -G 1 
 #SBATCH --time 12:00:00
-#SBATCH -o train_%j.out
+#SBATCH -o train_test_%j.out
 #SBATCH --mail-type END
 
 data_dir=data/buckeye
@@ -16,10 +16,9 @@ vocab_file=data/vocab.en.json
 model_dir=data/test_model
 
 module load conda/latest
-module load cuda/11.8
 
 conda activate ./env_cuda124
 
 # Wrapper script to train models 
-multipa-train --output_dir "$model_dir" --data_dir "$data_dir" --no_space --cache_dir "$dataset_cache" --use_gpu --num_train_epochs 2 --num_proc 12 \
+multipa-train --output_dir "$model_dir" --data_dir "$data_dir" --cache_dir "$dataset_cache" --use_gpu --num_train_epochs 2 --num_proc 8 \
     buckeye --train_samples 1000 --val_samples 200
