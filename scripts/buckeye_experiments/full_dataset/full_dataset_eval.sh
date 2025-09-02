65a2a8f772885d07e3065c4bf4b145f76ae156dc
Virginia Partridge <vcpartridge@umass.edu>
2025-09-02 22:51:59 +0000

    Full train dataset scripts added (no gender balancing)

--- scripts/buckeye_experiments/full_dataset/full_dataset_eval.sh
+++ scripts/buckeye_experiments/full_dataset/full_dataset_eval.sh
@@ -13,16 +13,22 @@
 # were used in training, so these models should have very good performance metrics, 
 # but the results aren't valid as a performance benchmark. 
 
-EVAL_RESULTS_CSV=data/evaluation_results/aggregate_metrics_final/full_dataset_eval.csv
-DETAILED_RESULTS_DIR=data/evaluation_results/detailed_predictions_final
+EVAL_RESULTS_CSV=data/evaluation_results/aggregate_metrics/full_dataset_eval.csv
+DETAILED_RESULTS_DIR=data/evaluation_results/detailed_predictions
 DATA_DIR=data/buckeye
 
 module load conda/latest
 conda activate ./env_cuda124
 
 multipa-evaluate --local_models \
- data/models/full_dataset_train_val_test \
- data/models/full_dataset_train_val \
+ data/models/full_dataset_train_val_test/wav2vec2-large-xlsr-53-buckeye-ipa \
+ data/models/full_dataset_train_val/wav2vec2-large-xlsr-53-buckeye-ipa \
+ data/models/full_dataset_train_val_bs32/wav2vec2-large-xlsr-53-buckeye-ipa \
+ data/models/full_dataset_train_1/wav2vec2-large-xlsr-53-buckeye-ipa \
+ data/models/full_dataset_train_2/wav2vec2-large-xlsr-53-buckeye-ipa \
+ data/models/full_dataset_train_3/wav2vec2-large-xlsr-53-buckeye-ipa \
+ data/models/full_dataset_train_4/wav2vec2-large-xlsr-53-buckeye-ipa \
+ data/models/full_dataset_train_5/wav2vec2-large-xlsr-53-buckeye-ipa \
  --eval_out $EVAL_RESULTS_CSV \
  --verbose_results_dir $DETAILED_RESULTS_DIR \
  --no_space --data_dir $DATA_DIR \
