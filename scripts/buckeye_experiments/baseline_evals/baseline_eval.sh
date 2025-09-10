df8a77b8a6aaea63b1b9046d19c595fc5fed93a8
Virginia Partridge <vcpartridge@umass.edu>
2025-09-10 00:10:58 +0000

    Added back espeak dependency to baseline eval

--- scripts/buckeye_experiments/baseline_evals/baseline_eval.sh
+++ scripts/buckeye_experiments/baseline_evals/baseline_eval.sh
@@ -16,9 +16,11 @@ DETAILED_RESULTS_DIR=data/evaluation_results/detailed_predictions
 DATA_DIR=data/buckeye
 
 module load conda/latest
+module load uri/main
+module load all/eSpeak-NG/1.50-gompi-2020a
 conda activate ./env_cuda124
 
-multipa-evaluate --hf_models facebook/wav2vec2-lv-60-espeak-cv-ft ctaguchi/wav2vec2-large-xlsr-japlmthufielta-ipa1000-ns \
+multipa-evaluate --hf_models facebook/wav2vec2-lv-60-espeak-cv-ft ctaguchi/wav2vec2-large-xlsr-japlmthufielta-ipa1000-ns  \
  --eval_out $EVAL_RESULTS_CSV \
  --verbose_results_dir $DETAILED_RESULTS_DIR \
  --no_space --data_dir $DATA_DIR \
