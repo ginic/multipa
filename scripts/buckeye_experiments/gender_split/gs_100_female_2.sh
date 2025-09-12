412dc923a9a80d841c1ab83e3c8d9a240e16dd71
Virginia Partridge <vcpartridge@umass.edu>
2025-09-12 18:28:33 +0000

    Updated gender eval scripts

---
+++ scripts/buckeye_experiments/gender_split/gs_100_female_2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH -c 8
+#SBATCH --mem=24GB
+#SBATCH -p gpu-preempt
+#SBATCH -G 4
+#SBATCH --constraint=vram40
+#SBATCH --nodes=1
+#SBATCH --time 3:00:00
+#SBATCH -o %j_gender_split_100_female_2.out
+#SBATCH --mail-type END
+
+batch_size=4
+grad_acc=2
+learning_rate=3e-4
+model_dir=data/models/gender_split_100_female_2
+
+dataset_cache=dataset_cache
+data_dir=data/buckeye
+
+
+module load conda/latest
+conda activate ./env_cuda124
+
+python --version
+
+multipa-train --output_dir "$model_dir" --data_dir "$data_dir" --cache_dir "$dataset_cache" --use_gpu --num_gpus 4 --num_train_epochs 10 --num_proc 8 \
+    --learning_rate $learning_rate --per_device_train_batch_size $batch_size --gradient_accumulation_steps $grad_acc --mask_time_length 4 \
+    --train_seed 119 \
+    buckeye --train_samples 4000 --val_samples 5605 --percent_female 1.0
--- scripts/buckeye_experiments/gender_split/gs_100_female_eval.sh
+++ scripts/buckeye_experiments/gender_split/gs_100_female_eval.sh
@@ -6,13 +6,13 @@
 #SBATCH -G 1
 #SBATCH --nodes=1
 #SBATCH --time 08:00:00
-#SBATCH -o %j_gender_split_70_female_eval.out
+#SBATCH -o %j_gender_split_100_female_eval.out
 #SBATCH --mail-type END
 
 
 # Evaluation results for our the models that change the data seed only 
 
-EVAL_RESULTS_CSV=data/evaluation_results/aggregate_metrics/gender_split_70_female_eval.csv
+EVAL_RESULTS_CSV=data/evaluation_results/aggregate_metrics/gender_split_100_female_eval.csv
 DETAILED_RESULTS_DIR=data/evaluation_results/detailed_predictions
 DATA_DIR=data/buckeye
 
@@ -20,11 +20,11 @@ module load conda/latest
 conda activate ./env_cuda124
 
 multipa-evaluate --local_models \
- data/models/gender_split_70_female_1/wav2vec2-large-xlsr-53-buckeye-ipa \
- data/models/gender_split_70_female_2/wav2vec2-large-xlsr-53-buckeye-ipa \
- data/models/gender_split_70_female_3/wav2vec2-large-xlsr-53-buckeye-ipa \
- data/models/gender_split_70_female_4/wav2vec2-large-xlsr-53-buckeye-ipa \
- data/models/gender_split_70_female_5/wav2vec2-large-xlsr-53-buckeye-ipa \
+ data/models/gender_split_100_female_1/wav2vec2-large-xlsr-53-buckeye-ipa \
+ data/models/gender_split_100_female_2/wav2vec2-large-xlsr-53-buckeye-ipa \
+ data/models/gender_split_100_female_3/wav2vec2-large-xlsr-53-buckeye-ipa \
+ data/models/gender_split_100_female_4/wav2vec2-large-xlsr-53-buckeye-ipa \
+ data/models/gender_split_100_female_5/wav2vec2-large-xlsr-53-buckeye-ipa \
  --eval_out $EVAL_RESULTS_CSV \
  --verbose_results_dir $DETAILED_RESULTS_DIR \
  --no_space --use_gpu --data_dir $DATA_DIR \
