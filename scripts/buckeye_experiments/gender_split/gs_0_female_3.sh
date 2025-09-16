ea2cfba696a4fa652e497af75228d31b44ff631e
Virginia Partridge <vcpartridge@umass.edu>
2025-09-16 16:58:32 +0000

    Only male speaker experiment results

--- scripts/buckeye_experiments/gender_split/gs_0_female_3.sh
+++ scripts/buckeye_experiments/gender_split/gs_0_female_3.sh
@@ -8,6 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --time 3:00:00
 #SBATCH -o %j_gender_split_0_female_3.out
+#SBATCH --exclude gpu010,gpu040,gpu038
 #SBATCH --mail-type END
 
 batch_size=4
@@ -26,5 +27,5 @@ python --version
 
 multipa-train --output_dir "$model_dir" --data_dir "$data_dir" --cache_dir "$dataset_cache" --use_gpu --num_gpus 4 --num_train_epochs 10 --num_proc 8 \
     --learning_rate $learning_rate --per_device_train_batch_size $batch_size --gradient_accumulation_steps $grad_acc --mask_time_length 4 \
-    --train_seed 814 \
+    --train_seed 809 \
     buckeye --train_samples 4000 --val_samples 5605 --percent_female 0.0 
